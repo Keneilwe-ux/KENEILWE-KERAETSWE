c5594e6dfcc0237209f22c93f339a1064dd7113e
Keneilwe-ux <76906124+Keneilwe-ux@users.noreply.github.com>
2022-02-23 01:14:27 +0200

    codetribe

---
+++ index.js
@@ -0,0 +1,8 @@
+<script>
+    
+var ME = document.getElementbyClass(ME);
+var bioBox = document.getElementbyClass(bioBox);
+var box = document.getElementbyClass(box);
+var phone = document.getElementbyClass(phone);
+
+</script>
